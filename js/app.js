e618ae5493ceaaf2af3737e9734645e264207088
Rafael Aldana <rafaelaldana6@Gmail.com>
2022-12-08 11:47:37 -0800

    added notes

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// function handleClick(event) {
+
+//   console.log('this is the event', event);
+//   console.log('this is the target', event.target);
+
+//   if (event.target.id === 'box-one') {
+//     let pElem = document.createElement('p');
+//     pElem.textContent = 'Box 1 was clicked!';
+//     parentElem.append(pElem);
+//   }
+// }
+
+
+// myContainer.addEventlist
